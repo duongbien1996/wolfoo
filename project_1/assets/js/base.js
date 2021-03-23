3926f2a3519ac8d3b07e86bf999a850a008e2c24
duongbien96 <duongbien1996@gmail.com>
2021-03-24 03:15:51 +0700

    update source

--- project_1/assets/js/base.js
+++ project_1/assets/js/base.js
@@ -1,5 +1,9 @@
 
-// $(document).ready(function () {
-    
-// });
+$(document).ready(function () {
+    // if ($( window ).width() <= 479) {
+    //     var get_dom_subsview_block = $('.subsview__block').get(0);
+    //     var get_dom_subsview_slide = $('.subsview__slide').get(0)
+    //     $('.home__subsview').append(get_dom_subsview_block,get_dom_subsview_slide);
+    // }
+});
 
--- project_1/assets/js/rules.js
+++ project_1/assets/js/rules.js
@@ -20,21 +20,48 @@ $(document).ready(function () {
     })
 
 
-    $('.owl__subsview').owlCarousel({
-        loop:true,
-        margin:56,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
+    // $('.owl__subsview').owlCarousel({
+    //     loop:true,
+    //     margin:56,
+    //     nav:true,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:3
+    //         },
+    //         1000:{
+    //             items:5
+    //         }
+    //     }
+    // })
+
+    /* Sử dụng cho thư viện swiper */
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 56,
+        pagination: {
+        //   el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20
             },
-            600:{
-                items:3
+            // when window width is >= 480px
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 30
             },
-            1000:{
-                items:5
+            // when window width is >= 640px
+            1000: {
+              slidesPerView: 5,
+              spaceBetween: 40
             }
         }
-    })
+    });
 
 });
